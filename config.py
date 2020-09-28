37d0b37e966b2b73b437d6ceee58b13f137d4d43
Ali Moghimi <ThisisJavid@Gmail.com>
2020-09-28 22:30:26 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 PORT = 443
 
+TO_CLT_BUFSIZE = 262144
+TO_TG_BUFSIZE = 262144
+
 # name -> secret (32 hex chars)
 USERS = {
-    "tg":  "00000000000000000000000000000001",
+    "tg":  "42aad8aa14b76f7bd88d381eeb5a896f",
     # "tg2": "0123456789abcdef0123456789abcdef",
 }
 
@@ -12,7 +15,7 @@ MODES = {
 
     # Makes the proxy harder to detect
     # Can be incompatible with very old clients
-    "secure": False,
+    "secure": True,
 
     # Makes the proxy even more hard to detect
     # Can be incompatible with old clients
@@ -24,4 +27,4 @@ MODES = {
 # TLS_DOMAIN = "www.google.com"
 
 # Tag for advertising, obtainable from @MTProxybot
-# AD_TAG = "3c09c680b76ee91a4c25ad51f742267d"
+AD_TAG = "f66d4f6e5d64bf22f9b1782df72a398b"
